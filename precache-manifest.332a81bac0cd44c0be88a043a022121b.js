10d447147eb2fa3148c9ff1287431cd95d38f9fd
gokul <asdf1234@>
2021-01-06 11:42:55 +0530

    Updates

---
+++ precache-manifest.332a81bac0cd44c0be88a043a022121b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd61a77c62958460d20e44122b7fdee",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "d8bf7c9e9c2c63359ac6",
+    "url": "/covid_tracker/static/css/main.f3c59c85.chunk.css"
+  },
+  {
+    "revision": "b73e2699e52e002348e9",
+    "url": "/covid_tracker/static/js/2.6eb61c41.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_tracker/static/js/2.6eb61c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bf7c9e9c2c63359ac6",
+    "url": "/covid_tracker/static/js/main.7f0d1b8f.chunk.js"
+  },
+  {
+    "revision": "40b6635e799cef607ea1",
+    "url": "/covid_tracker/static/js/runtime-main.2a3016a8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
